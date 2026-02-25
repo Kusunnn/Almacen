9f7b461b4fce0cbd22b3886e8c119adb42644e77
Mario Diaz <mariod1452@gmail.com>
2026-02-25 12:42:43 -0600

    db con prisma func

--- Backend/src/db/prisma.js
+++ Backend/src/db/prisma.js
@@ -1,3 +1,9 @@
+import dotenv from "dotenv";
+import dns from "node:dns";
+import { fileURLToPath } from "node:url";
 import { PrismaClient } from "@prisma/client";
+
+dotenv.config({ path: fileURLToPath(new URL("../../.env", import.meta.url)) });
+dns.setDefaultResultOrder("ipv4first");
 
 export const prisma = new PrismaClient();
--- Backend/src/db/sql.js
+++ Backend/src/db/sql.js
@@ -1,6 +1,9 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import dns from "node:dns";
 import postgres from "postgres";
+import { fileURLToPath } from "node:url";
+
+dotenv.config({ path: fileURLToPath(new URL("../../.env", import.meta.url)) });
 
 // Si tu red no rutea IPv6 bien (tu caso), esto evita que Node elija AAAA primero
 dns.setDefaultResultOrder("ipv4first");
