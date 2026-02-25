4afd1fa06e0212cf67fabf6568f5a27b276ce926
OswaldoFloresA95 <oswaldoo_95@hotmail.com>
2026-02-25 16:38:06 -0600

    conexion db

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,17 @@
-console.log("Hola Mundo");
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function probarDatos() {
+    try {
+        const todosLosUsuarios = await prisma.usuarios.findMany();
+        console.log('--- Conexión con Datos Reales OK ---');
+        console.table(todosLosUsuarios);
+    } catch (error) {
+        console.error('Error al leer de Supabase:', error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+
+probarDatos();
